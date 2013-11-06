8e5e4a5af781b99ecdc62907a88ca3117e0a5860
Antanas Uršulis <antanas.ursulis@gmail.com>
2013-11-07 01:11:01 +0200

    Routing by annotations just makes more sense

--- src/AB/Bundle/Controller/DefaultController.php
+++ src/AB/Bundle/Controller/DefaultController.php
@@ -3,9 +3,13 @@
 namespace AB\Bundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController extends Controller
 {
+    /**
+     * @Route("/hello/{name}", name="_hello")
+     */
     public function indexAction($name)
     {
         return $this->render('MainBundle:Default:index.html.twig', array('name' => $name));
