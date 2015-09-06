f950753cf2ab0213f965894d24745f0200e12984
Edgar Liberis <edgaras.liberis@gmail.com>
2015-09-06 19:59:21 +0100

    JS group updates

--- src/AB/Bundle/Resources/public/js/groups.js
+++ src/AB/Bundle/Resources/public/js/groups.js
@@ -85,7 +85,14 @@ ApiComms.prototype.updateGroup = function(id, group, onSuccess) {
 var GroupApp = function() {
     var self = this;
     this.comms = new ApiComms();
-    this.comms.listGroups(self.loadGroups);
+
+    // We assume users don't change during the grouping process
+    this.comms.listUsers(function (users) {
+        self.pupils = users["pupils"];
+        self.mentors = users["mentors"];
+    });
+
+    this.comms.listGroups(self.loadGroups.bind(self));
 }
 
 GroupApp.prototype.showError = function(msg) {
@@ -93,12 +100,90 @@ GroupApp.prototype.showError = function(msg) {
 }
 
 GroupApp.prototype.loadGroups = function(groupObj) {
+    var self = this;
+    self.groups = groupObj.groups;
     Object.keys(groupObj.groups).forEach(function (id) {
         var group = groupObj.groups[id];
-        // TODO: set up a card for group #id
-        console.log("Setting up card for group #" + id + " with mentor " + (group.mentor? group.mentor.email : "(unknown)"));
+        console.info("Setting up card for group #" + id + " with mentor " + (group.mentor? group.mentor.email : "(unknown)"));
+        self.displayGroup(id, group);
     });
 };
+
+GroupApp.prototype.deleteAllGroups = function() {
+    var self = this;
+    self.comms.deleteAllGroups(function() {
+        $(".group-card-wrapper").delete();
+        self.groups = [];
+    });
+}
+
+GroupApp.prototype.deleteGroup = function() {
+
+}
+
+GroupApp.prototype.displayGroup = function(id, group) {
+    var self = this;
+
+    // Add group html
+    var groupHtml = '\
+    <div id="group-card-{id}" data-id="{id}" class="group-card-wrapper col-sm-4 col-md-3">
+      <div class="group-card panel-shadow"> \
+        <h3>Grupė #{id}</h3> \
+        <div class="form-group"> \
+          <label for="select-mentor-{id}">Mentorius</label> \
+          <select class="form-control" id="select-mentor-{id}"> \
+          </select> \
+        </div> \
+        <div class="form-group"> \
+          <label for="select-secondary-mentor-{id}">Pagalbinis mentorius</label> \
+          <select class="form-control" id="select-secondary-mentor-{id}"> \
+          </select> \
+        </div> \
+        <div class="form-group"> \
+          <label for="select-pupils-{id}">Mokiniai</label> \
+          <select class="form-control" id="select-pupils-{id}" multiple="multiple"> \
+          </select> \
+        </div> \
+        <div class="form-group"> \
+          <button type="button" class="btn btn-success form-control"> \
+            <span class="glyphicon glyphicon-ok"></span> Išsaugoti \
+          </button> \
+          <button type="button" class="btn btn-danger form-control"> \
+            <span class="glyphicon glyphicon-remove"></span> Pašalinti \
+          </button> \
+        </div> \
+      </div> \
+    </div>'.replace(/{id}/g, id);
+    $("#groups-container").append(groupHtml);
+
+    // Set up select values
+    var $mentorSelect = $("#select-mentor-" + id), $secMentorSelect = $("#select-secondary-mentor-" + id);
+    self.mentors.forEach(function (mentor) {
+        var optionStr = '<option {selected} data-id={id}>{name}</option>'
+            .replace(/{id}/g, mentor.id)
+            .replace(/{name}/g, mentor.name);
+        $mentorSelect.append(optionStr
+            .replace(/{selected}/g, group.mentor !== undefined && mentor.id == group.mentor.id? "selected" : ""));
+        $secMentorSelect.append(optionStr
+            .replace(/{selected}/g, group.secondaryMentor !== undefined && mentor.id == group.secondaryMentor.id? "selected" : ""));
+        
+    });
+
+    var $pupilSelect = $("#select-pupils-" + id);
+    var pupilIds = group.pupils.map(function(p) { return p.id; });
+    self.pupils.forEach(function (pupil) {
+        var optionStr = '<option {selected} data-id={id}>{name}</option>'
+            .replace(/{id}/g, pupil.id)
+            .replace(/{name}/g, pupil.name)
+            .replace(/{selected}/g, pupilIds.indexOf(pupil.id) != -1? "selected" : "");
+        $pupilSelect.append(optionStr);
+    });
+
+    $('#group-card-'+id+' select').select2();
+
+    // Set up button callbacks
+    
+}
 
 var app = new GroupApp();
 $(document).ajaxError(function (err) {
